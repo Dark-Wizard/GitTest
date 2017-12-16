4b34abd7ac2334e8b655b5c474dbfc7e84ee3c54
Dark-wizard <bellaajiheb@gmail.com>
2017-12-16 02:05:30 +0100

    hello

--- app/src/main/java/com/example/dell/gittest/MainActivity.java
+++ app/src/main/java/com/example/dell/gittest/MainActivity.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-int a;
+int a, karim_ya3tih_3asba=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         a=0;
+        karim_ya3tih_3asba=0;
     }
 }
