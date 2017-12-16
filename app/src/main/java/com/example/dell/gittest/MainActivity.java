e0cc81648f385ada38ff15ed64beb36a8d9d1781
bahri <othmeneb@gmail.com>
2017-12-16 02:40:53 +0100

    ya iheb ya 7zin

--- app/src/main/java/com/example/dell/gittest/MainActivity.java
+++ app/src/main/java/com/example/dell/gittest/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 int a, karim_ya3tih_3asba=3,oss=6;
-String ihebta7aan="bhim";
+String ihebta7aan="bhim",ki;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
